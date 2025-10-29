3d3e459dacf3de1584afcf71e0d4808efe540808
JMK <2206116@students.kcau.ac.ke>
2025-10-29 12:28:16 +0300

    Create clientsyncfixscript.cs

---
+++ clientsyncfixscript.cs
@@ -0,0 +1,16 @@
+using RageCoop.Client.Scripting;
+
+namespace RageCoop.Resources.SyncFix
+{
+    public class Main : ClientScript
+    {
+        public override void OnStart()
+        {
+            // Initiate your script here
+        }
+        public override void OnStop()
+        {
+            // Free all resources and perform cleanup
+        }
+    }
+}
